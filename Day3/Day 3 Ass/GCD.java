996d8ccf2e85d98a342ec842f058707a90ad1011
vilasdasalkar <vilasdasalkar@gmail.com>
2024-07-31 16:10:46 +0530

    Problem day 3 / GCD Problem

---
+++ Day3/Day 3 Ass/GCD.java
@@ -0,0 +1,22 @@
+import java.util.*;
+//7. Write a program to find greatest common divisor (GCD) or highest common factor (HCF) of given two numbers
+
+public class GCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        while (num2 != 0) {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+
+        System.out.println("GCD: " + num1);
+    }
+}
