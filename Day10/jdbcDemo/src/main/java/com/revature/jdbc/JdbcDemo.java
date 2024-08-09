1080f62cea5e04d039ab8c0f396b2107ef630608
vilasdasalkar <vilasdasalkar@gmail.com>
2024-08-09 12:17:54 +0530

    Day 10 jdbc and sql trigger and stored function we practies

--- Day10/jdbcDemo/src/main/java/com/revature/jdbc/JdbcDemo.java
+++ Day10/jdbcDemo/src/main/java/com/revature/jdbc/JdbcDemo.java
@@ -1,5 +1,5 @@
 package com.revature.jdbc;
-
+import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,14 +10,27 @@ import java.sql.Statement;
 
 public class JdbcDemo {
 	public static void main(String arg[]) {
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("enter url");
+	String url=sc.nextLine();
+	
+
+	System.out.println("enter user name");
+	String uname=sc.nextLine();
+	
+
+	System.out.println("enter url");
+	String pwd=sc.next();
+	
 	
 	Connection con=null;
 	Statement statement=null;
 	ResultSet resultSet=null;
     
-	String url="jdbc:mysql://localhost:3306/revainfo";
-	String uname="root";
-	String pwd="root";
+	//String url="jdbc:mysql://localhost:3306/revainfo";
+	//String uname="root";
+	//String pwd="root";
 	
 	try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
